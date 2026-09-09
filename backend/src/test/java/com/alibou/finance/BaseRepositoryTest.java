4906f78031310683e75bde5643bb1e0c9e5b4a4d
rivohery <rivoherydev01@gmail.com>
2026-09-09 23:08:20 +0300

    fix: modify BaseRepositoryTest

--- backend/src/test/java/com/alibou/finance/BaseRepositoryTest.java
+++ backend/src/test/java/com/alibou/finance/BaseRepositoryTest.java
@@ -5,5 +5,5 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest
-public abstract class BaseRepositoryTest extends JpaConfigurationTest{
+public abstract class BaseRepositoryTest extends DBProfileTest{
 }
