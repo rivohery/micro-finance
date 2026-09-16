0b13b9a11e741443007be486ac6e1a36736599c3
rivohery <rivoherydev01@gmail.com>
2026-09-16 13:12:32 +0300

    feat: Finish test of addMonthlyInterestUseCase

--- backend/src/test/java/com/alibou/finance/BaseIntegrationTest.java
+++ backend/src/test/java/com/alibou/finance/BaseIntegrationTest.java
@@ -4,5 +4,5 @@ package com.alibou.finance;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-public abstract class BaseIntegrationTest extends AbstractH2IntegrationTest{
+public abstract class BaseIntegrationTest extends AbstractTestcontainersIT{
 }
--- backend/src/test/java/com/alibou/finance/BaseRepositoryTest.java
+++ backend/src/test/java/com/alibou/finance/BaseRepositoryTest.java
@@ -3,5 +3,5 @@ package com.alibou.finance;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-public abstract class BaseRepositoryTest extends AbstractH2IntegrationTest{
+public abstract class BaseRepositoryTest extends AbstractTestcontainersIT{
 }
